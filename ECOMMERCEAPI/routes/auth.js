8c1b89de823f40d0d8c16de3641a3c29dedbc4d2
Ricky Huang <rickyhuang0824@gmail.com>
2021-11-14 20:20:22 -0800

    Added routes for auth

---
+++ ECOMMERCEAPI/routes/auth.js
@@ -0,0 +1,49 @@
+const router = require("express").Router()
+const User = require("../models/User")
+const CryptoJS = require("crypto-js")
+
+router.post("/register", async (req, res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString(),
+    })
+
+    try {
+        const savedUser =  await newUser.save()
+        res.status(201).json(savedUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        if (!user) {
+            return res.status(401).json("Wrong Credential")
+        }
+    
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        )
+    
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+    
+        if (originalPassword !== req.body.password) {
+            return res.status(401).json("Wrong Credential")
+        }
+
+        const  { password, ...other} = user._doc
+
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
