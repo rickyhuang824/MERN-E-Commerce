a7e8828e04c1c59e9bc69ece8a9189b37179e289
Ricky Huang <rickyhuang0824@gmail.com>
2021-11-14 23:06:11 -0800

    Added router for admin

--- ECOMMERCEAPI/routes/user.js
+++ ECOMMERCEAPI/routes/user.js
@@ -1,7 +1,8 @@
 const router = require("express").Router()
 const User = require("../models/User")
-const {verifyTokenAndAuthorization} = require("./verifyToken")
+const {verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken")
 
+// Update
 router.put("/:id", verifyTokenAndAuthorization, async (req, res)=>{
     try {
         const updatedUser =  await User.findByIdAndUpdate(
@@ -16,6 +17,39 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res)=>{
         res.status(500).json(err)
     }
 
+})
+
+// Delete
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id) 
+        res.status(200).json("User has been deleted...")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// Get
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const { password, ...other} = user._doc
+        res.status(200).json(other)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const queryNew = req.query.new
+        const users = queryNew 
+            ? await User.find().sort({ _id: -1 }).limit(5)
+            : await User.find()
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
