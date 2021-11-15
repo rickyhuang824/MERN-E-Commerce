4b282c4c8a754dbbdc3d6dc637158f4356f6d79c
Ricky Huang <rickyhuang0824@gmail.com>
2021-11-14 21:35:04 -0800

    Added user routes for update info

--- ECOMMERCEAPI/routes/user.js
+++ ECOMMERCEAPI/routes/user.js
@@ -1,8 +1,21 @@
 const router = require("express").Router()
+const User = require("../models/User")
 const {verifyTokenAndAuthorization} = require("./verifyToken")
 
-router.put("/:id", verifyTokenAndAuthorization, (req, res)=>{
-    
+router.put("/:id", verifyTokenAndAuthorization, async (req, res)=>{
+    try {
+        const updatedUser =  await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        )
+        res.status(200).json(updatedUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
 })
 
 module.exports = router
