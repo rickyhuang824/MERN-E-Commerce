d1e6bdb993580f9412ad9d7f91730caacb938734
Ricky Huang <rickyhuang0824@gmail.com>
2021-11-14 23:27:23 -0800

    Added stats for user route

--- ECOMMERCEAPI/routes/user.js
+++ ECOMMERCEAPI/routes/user.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const User = require("../models/User")
+const { route } = require("./auth")
 const {verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken")
 
 // Update
@@ -50,6 +51,27 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
     } catch (err) {
         res.status(500).json(err)
     }
+})
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date()
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1))
+
+    try {
+       const data = await User.aggregate([
+           { $match: { createdAt: { $gte: lastYear }}},
+           { $project: { month: { $month: "$createdAt" }}},
+           { $group: {
+               _id: "$month",
+               total: { $sum: 1 }
+           }}
+       ]) 
+
+       res.status(200).json(data)
+    } catch (err) {
+       res.status(500).json(err) 
+    }
+
 })
 
 module.exports = router
