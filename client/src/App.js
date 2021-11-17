7cc356b10c546fa78db6c823b355cfc7373c9971
Ricky Huang <rickyhuang0824@gmail.com>
2021-11-16 23:35:52 -0800

    Use Router to route diffrent pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,27 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom"
 
 function App() {
+
+  const user = true
   return (
-    <ProductList />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="products/*" element= { <ProductList /> } />
+        <Route path="/product/:id" element={ <Product /> }/>
+        <Route path="cart" element= { <Cart /> } /> 
+        <Route path="login" element= { user ? <Navigate to="/" /> : <Login /> } /> 
+        <Route path="register" element= { user ? <Navigate to="/" /> : <Register /> } /> 
+      </Routes>
+    </Router>
   )
 }
 
