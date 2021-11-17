b6955f4c30e68dbe56f0cf781ffd7299e301c914
Ricky Huang <rickyhuang0824@gmail.com>
2021-11-16 23:37:54 -0800

    Added axios object request method

---
+++ client/src/requestMethods.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080/api/"
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTFmNzdjYzgwYWViNTQ4MWNiNzU4NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzEzMTYwMywiZXhwIjoxNjM3MzA0NDAzfQ.YmWzp7gZnDrllGyaRM1BC3lFVY-rNb69s6NIhK0URHs"
+
+export const publicRequest = axios.create({
+    baseURL: BASE_URL,
+})
+
+export const userRequest = axios.create({
+    baseURL: BASE_URL,
+    header: {token: `Bearer ${TOKEN}`}
+})
